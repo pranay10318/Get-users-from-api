705a41d92bc6a1aa16cb4f05deba973f7bb1eacb
pranay10318 <109957442+pranay10318@users.noreply.github.com>
2022-11-20 11:32:26 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [users, setUser] = useState([]);
+
+  const loadusers = async () => {
+    console.log("before fetch");
+    const response = await fetch("https://api.github.com/users");
+    //i have not used the api you have mentioned as it was not in correct json format
+    const jsonResponse = await response.json();
+    setUser(jsonResponse);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Fectching data from api</h1>
+      <div className="btn">
+        <button onClick={loadusers}>GET DATA</button>
+      </div>
+      <div className="fetch-container">
+        {users.map(({ id, login, node_id, avatar_url }) => (
+          <div className="fetch-card" key={id}>
+            <b>Login</b>&nbsp;&nbsp;:{login}
+            <br />
+            <b>Node_id</b>:&nbsp;&nbsp;{node_id}
+            <br />
+            <b>Avatar_url</b>&nbsp;&nbsp;:{avatar_url}
+          </div>
+        ))}
+      </div>
+      {console.log("users " + users.length)}
     </div>
   );
 }
